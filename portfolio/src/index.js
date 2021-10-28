efb9d7b63f47db7925e4a7b2d2fb9456972802fd
scottskinn <84105761+scottskinn@users.noreply.github.com>
2021-10-28 18:56:53 -0400

    add hashrouter to index.js

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
